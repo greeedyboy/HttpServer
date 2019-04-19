e35d28b164b8ffd9bc2f8e36a1aada1410d3a37c
greedyboy <greedyboy@163.com>
2019-04-20 02:16:32 +0800

    w

--- p.py
+++ p.py
@@ -30,11 +30,17 @@ def gitpush(fn,commit='by q',delfn=True):
     # os.system('cd .deploy_git')
     # os.system('rm -rf .git')
     # os.system('cd ../')
-
-    print('clone git')
-    os.system('git clone https://'+ coref +' cfn')
     print('entre the cfn')
     os.system('cd cfn')
+    print('git config user name')
+    os.system('git config user.email "greedyboy@163.com"')
+    os.system('git config user.name "greedyboy"')
+    
+    print('回到主目录')
+    os.system('cd ../')
+    print('clone git')
+    os.system('git clone https://'+ coref +' cfn')
+
     print('checkout master')
 
     os.system('git checkout master')
@@ -43,10 +49,7 @@ def gitpush(fn,commit='by q',delfn=True):
     print('移动 主目录blog文件到后面')
     os.system('mv ./'+ fn + ' ./cfn/'+fn)
 
-    os.system('cd cfn')
-    print('git config user name')
-    os.system('git config user.email "greedyboy@163.com"')
-    os.system('git config user.name "greedyboy"')
+
     print('git add commit')
     os.system('git add '+fn)
     os.system('git commit -m "'+ commit +'"')
