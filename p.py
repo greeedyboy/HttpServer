d4bc919bfb73d50b96fe98c1e42a627566e5b8a7
greedyboy <greedyboy@163.com>
2019-04-21 01:31:00 +0800

    Update p.py

--- p.py
+++ p.py
@@ -36,7 +36,7 @@ def gitpush(fn,commit='by q',delfn=True):
 
 
     print('clone git')
-    os.system('git clone https://'+ coref +' cfn')
+    os.system('git clone https://github.com/greedyboy/HttpServer.git cfn')
 
     sleep(20)
 
@@ -56,8 +56,8 @@ def gitpush(fn,commit='by q',delfn=True):
     os.system('git checkout master')
     print('回到主目录')
     os.system('cd ../')
-    print('移动 主目录blog文件到后面')
-    os.system('mv ./'+ fn + ' ./cfn/'+fn)
+    #print('移动 主目录blog文件到后面')
+    #os.system('mv ./'+ fn + ' ./cfn/'+fn)
 
 
     print('git add commit')
