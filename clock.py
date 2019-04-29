59ab23bbc71d4e95b5ba5376318143c11eb1252e
greeedyboy <49837065+greeedyboy@users.noreply.github.com>
2019-04-29 10:04:53 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -10,7 +10,7 @@ import subprocess
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=120)
+@sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('This job is run every five minutes.')
 
